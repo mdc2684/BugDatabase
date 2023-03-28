7720791acfe3cfa31289663b3e03616d89c2de98
meadding231 <66891531+meadd231@users.noreply.github.com>
2023-03-28 11:05:25 +0900

    home 기본 구조

--- app.py
+++ app.py
@@ -2,110 +2,67 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://sparta:test@cluster0.gya4p0t.mongodb.net/?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://sparta:test@cluster0.ia8rqcv.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
 
-@app.route("/register", methods=["POST"])
-def register():
-    userid_receive = request.form['userid_give']
-    name_receive = request.form['name_give']
-    pwd_receive = request.form['pwd_give']
-    email_receive = request.form['email_give']
-    tel_receive = request.form['tel_give']
-    
+@app.route("/bug", methods=["POST"])
+def movie_post():
+    title_receive = request.form['title_give']
+    category_receive = request.form['category_give']
+    content_receive = request.form['content_give']
+
+    last_bug_id = db.auto_increment.find_one()['bug_id']
+    db.auto_increment.update_one({'bug_id':last_bug_id}, {'$set':{'bug_id':last_bug_id+1}})
 
     doc = {
-        'userid':userid_receive,
-        'name':name_receive,
-        'pwd':pwd_receive,
-        'email':email_receive,
-        'tel':tel_receive
-    }
-    db.user.insert_one(doc)
-    return jsonify({'msg': '회원가입 완료!'})
-
-@app.route("/register", methods=["GET"])
-def register_form():
-   return render_template('register.html')
-
-@app.route("/login", methods=["GET"])
-def login_form():
-   return render_template('login.html')
-
-@app.route("/login", methods=["POST"])
-def login():
-   return jsonify({'msg': '로그인 완료!'})
-
-
-
-
-@app.route("/write", methods=["POST"])
-def board_write():
-   
-   count = list(db.board.find({}, {'_id': False}))
-   seq = len(count) + 1
-
-   title_receive = request.form['title_give']
-   userid_receive = request.form['userid_give']
-   comment_receive = request.form['comment_give']
-
-
-
-   doc = {
-        'seq' : seq,
+        'id':last_bug_id+1,
         'title':title_receive,
-        'userid':userid_receive,
-        'comment':comment_receive
-        
-   }
-   
-   db.board.insert_one(doc)
-   return jsonify({'msg': '게시글 작성 완료!'})
+        'category':category_receive,
+        'content':content_receive,
+        'user_id':1,
+        'user_nickname':'마태현(임시)'
+    }
+    db.bugs.insert_one(doc)
 
+    return jsonify({'msg':'저장 완료!'})
 
-@app.route("/write", methods=["GET"])
-def write_form():
-   return render_template('write.html')
+@app.route("/bug_update", methods=["POST"])
+def bug_update():
+    title_receive = request.form['title_give']
+    category_receive = request.form['category_give']
+    content_receive = request.form['content_give']
+    bug_id_receive = int(request.form['bug_id_give'])
 
-@app.route('/board')
-def board():
-   boards = list(db.board.find({},{'_id':False}))
-   return jsonify({'result': boards})
+    doc = {
+        'id':bug_id_receive,
+        'title':title_receive,
+        'category':category_receive,
+        'content':content_receive,
+        'user_id':1,
+        'user_nickname':'마태현(임시)'
+    }
+    db.bugs.update_one({'id':bug_id_receive}, {'$set':doc})
 
-
-
-
-
-@app.route("/delete", methods=["POST"])
-def board_deletevalue():
-   seq = request.form['seq']
-   
-   db.board.delete_one({'seq':int(seq)})
-   return jsonify({'msg': '삭제 완료!'})
-
+    return jsonify({'msg':'수정 완료!'})
 
 
 
-@app.route('/update/<int:num>', methods=['POST'])
-def update_post():
-    seq = request.form['seq']
-    num_receive = request.form['num_give']
-    db.board.update_one({'num': int(num_receive)})
-    return jsonify({'msg': '수정 완료'})
+@app.route("/bug", methods=["GET"])
+def movie_get():
+    all_bugs = list(db.bugs.find({},{'_id':False}))
+    return jsonify({'result':all_bugs})
 
-@app.route("/update", methods = ['GET'])
-def board_updateform():
-   
-   return render_template('update.html')
-
-
-@app.route("/modify", methods=["GET"])
-def modify_form():
-   return render_template('modify.html')
+@app.route('/modify', methods=["POST"])
+def modify_bug():
+    bug_id_receive = request.form['bug_id_give']
+    print(bug_id_receive)
+    target_bug = db.bugs.find_one({'id':int(bug_id_receive)}, {'_id':False})
+    # target_bug = {'id':bug_id_receive, 'title':'몰라'}
+    return jsonify({'result':target_bug})
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
